f38e129b0bddd7713f939222c7af406576c1f590
alimjanovf <almjnvfsystem@gmail.com>
2023-03-23 16:44:02 +0500

    game

---
+++ GameConsoleApp/GameTask4ConsoleApp/Generator.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Channels;
+
+namespace GameTask4ConsoleApp
+{
+    public  class Generator
+    {
+        public static string GenerateHmacKey(List<string> moves , int computerMove)
+        {
+            var bytes = BitConverter.GetBytes(computerMove);
+            var key = BitConverter.ToString(bytes).Replace("-", "");
+
+            var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key));
+            var hmacKey = hmac.ComputeHash(Encoding.UTF8.GetBytes(moves[computerMove]));
+            
+            return BitConverter.ToString(hmacKey).Replace("-", "");
+        }
+
+         
+    }
+}
---
+++ GameConsoleApp/GameTask4ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+using GameTask4ConsoleApp;
+using System.Text.RegularExpressions;
+using Validator = GameTask4ConsoleApp.Validator;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //while (true)
+        //{
+        var input = Regex.Replace(Console.ReadLine(), @"\s+", " ")
+            .Split()
+            .ToList();
+        
+        try
+        {
+                var resultOfValidating = Validator.StartToValidate(input);
+
+                while (resultOfValidating)
+                {
+                    var compMove = RandomNumberGenerator.GetInt32(1,input.Count+1);
+                    
+                    GameLogic game = new GameLogic(input.Count);
+
+                   
+                    if (resultOfValidating) Table.ShowGameTable(input);
+                    
+                    var option = Console.ReadLine().Replace(" ", "");
+                    if (option == "?")
+                        Table.ShowHelpMenu(input);
+                    
+                    //Validator.StartToValidate(option,input.Count);
+
+                    if(option == "0")
+                        break; 
+                    
+                    if (int.Parse(option) <= input.Count)
+                        game.FindWinner(int.Parse(option));
+                }
+        }
+        catch (Exception e) 
+        { 
+            Console.WriteLine(e.Message);
+        }
+    }
+}
